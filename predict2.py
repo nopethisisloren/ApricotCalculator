5a6b3988fc6c88d41e8995fab91703a58e48a71d
nopethisisloren <64239366+nopethisisloren@users.noreply.github.com>
2023-04-20 11:24:34 -0500

    Add files via upload

--- predict2.py
+++ predict2.py
@@ -8,8 +8,8 @@ def load_data(sheets_url):
     csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
     return pd.read_csv(csv_url)
 
-aita = load_data(st.secrets["public_gsheets_url"])
-aita.dropna()
+aita = load_data(st.secrets["public_gsheets_url"]).dropna()
+
 
 #build model
 import sklearn
